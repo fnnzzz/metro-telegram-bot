484141581cc0a5acbdad7c7de4499246168df714
Vlad Feninets <v.feninets@gmail.com>
2020-06-10 16:33:59 +0300

    add script for deployment

---
+++ deploy.js
@@ -0,0 +1,24 @@
+const { exec } = require("child_process");
+
+exec("docker build --rm -t metrobot .", (err, stdout) => {
+  if (err) {
+    console.log(err);
+  }
+
+  exec("docker stop metrobot-container || true", (err, stdout) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(stdout);
+
+    exec(
+      "docker run -d --rm -e BOT_TOKEN=${BOT_TOKEN} -p 80:80 --name metrobot-container metrobot",
+      (err, stdout) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(stdout);
+      }
+    );
+  });
+});
